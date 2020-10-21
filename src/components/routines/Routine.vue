9056291ddd7a10fca43cfdeaa489549def71849a
Bruno <brunovolko@gmail.com>
2020-10-21 11:38:33 -0300

    Mas cosas en vista de rutina

--- src/components/routines/Routine.vue
+++ src/components/routines/Routine.vue
@@ -19,6 +19,46 @@
             <v-col>
               <div class="text-h6 font-weight-bold">Categoría: {{ categoria }}</div>
             </v-col>
+            <v-col>
+              <v-rating
+                  :color="color"
+                  :background-color="color"
+                  empty-icon="mdi-star-outline"
+                  full-icon="mdi-star"
+                  half-icon="mdi-star-half-full"
+                  half-increments
+                  length="5"
+                  readonly
+                  size="28"
+                  :value="rating"
+                  medium
+                  dense
+              ></v-rating>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="2">
+              <v-avatar>
+                <v-img v-bind:key="creator" :src="creator.avatarUrl" :alt="'Avatar de ' + creator.username"  contain></v-img>
+              </v-avatar>
+            </v-col>
+            <v-col cols="10">
+              <div class="text-h6 font-weight-black">{{ creator.username }}</div>
+            </v-col>
+          </v-row>
+          <!--<v-row>
+            <v-col>
+              <span class="text-h6 mr-2">{{ time }}'</span>
+              <img src="../assets/stopwatch.png" width="28px" height="28px" />
+            </v-col>
+            <v-col>
+
+            </v-col>
+          </v-row>-->
+          <v-row>
+            <v-col>
+              <div class="text-subtitle-1 mt-5">{{ detail }}</div>
+            </v-col>
           </v-row>
         </v-col>
       </v-row>
@@ -48,13 +88,20 @@ export default {
 name: "Routine",
   data() {
     return {
-      /*store: SectionStore,
-      name: '',
-      category:'',
-      duration: 0*/
+      /*store: SectionStore*/
       id_routine: this.$route.params.id_routine,
       title: 'Este es el título de la rutina',
       categoria: 'Piernas',
+      detail: 'Con esta rutina vas a quedar nivel Hulk, no te digo que vas a sacar algo de musculito, sino que vas a quedar con TREMENDAS PIERNAS BRO. Si, así como lees. ¿Estoy escribiendo para rellenar? Quizás. Es importante? No. Los invito a suscribirse a mi canal y dejar su comentario.',
+      rating: 4.5,
+      creator: {
+        id: 3,
+        username: "johndoe7",
+        gender: "male",
+        avatarUrl: "https://www.pngarts.com/files/3/Avatar-PNG-Download-Image.png",
+        "dateCreated": 1602139940660,
+        "dateLastActive": 1602646870971
+      },
       cycles: [
         { title: 'Calentamiento', qty: 1,
           exercises: [
