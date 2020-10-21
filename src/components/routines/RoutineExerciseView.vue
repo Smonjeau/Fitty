bded39ec34bf65f68b8b3bcd6316189ad346bb26
Bruno <brunovolko@gmail.com>
2020-10-21 00:29:17 -0300

    Boton para ver video dentro de un dialog

--- src/components/routines/RoutineExerciseView.vue
+++ src/components/routines/RoutineExerciseView.vue
@@ -3,7 +3,44 @@
     <v-row>
       <v-col>{{ title }}</v-col>
       <v-col>x {{ qty }}</v-col>
+      <v-col class="text-right">
+
+        <v-dialog
+            v-model="dialog"
+            max-width="700"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-icon
+                medium
+                v-bind="attrs"
+                v-on="on">mdi-play-circle-outline</v-icon>
+          </template>
+          <v-card>
+            <v-card-title class="headline">
+              <v-col>
+                Video demostrativo
+              </v-col>
+              <v-col class="text-right">
+                <v-btn
+                    color="green darken-1"
+                    text
+                    @click="dialog = false"
+                >
+                  Listo
+                </v-btn>
+              </v-col>
+            </v-card-title>
+            <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+            ></v-img>
+          </v-card>
+        </v-dialog>
+
+
+
+      </v-col>
     </v-row>
+
   </div>
 </template>
 
